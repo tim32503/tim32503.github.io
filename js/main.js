473ce3ab065d21d572dfd5594ba8080de876c496
Tim Hsueh <timhsueh@curihaosity.xyz>
2024-01-02 15:01:29 +0800

    網站更新: 2024年1月2日 15:01:29

--- js/main.js
+++ js/main.js
@@ -842,6 +842,26 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
+  let disqus = document.querySelector('#disqus_thread')
+  const removeAds = setInterval(() => {
+    let iframes = document.querySelectorAll('iframe')
+
+    for (var iframe in iframes) {
+      if (typeof iframes[iframe].src === 'undefined') {
+        continue;
+      }
+
+      if (iframes[iframe].src.match(/(ads-iframe)|(disqusads)/gi)) {
+        iframes[iframe].style.display = 'none';
+        disqus.style.width = '100%';
+      }
+    }
+  }, 500)
+
+  setTimeout(() => {
+    clearInterval(removeAds)
+  }, 5000)
+
   window.refreshFn = function () {
     initAdjust()
 
